6742469f30c04e2fe85cacd2c8660ff9447fb612
GastonNicolasBaez <115502959+GastonNicolasBaez@users.noreply.github.com>
2022-10-31 20:24:13 -0300

    corss

--- src/main/java/com/portfoliobaez/gnb/GnbApplication.java
+++ src/main/java/com/portfoliobaez/gnb/GnbApplication.java
@@ -18,7 +18,7 @@ public class GnbApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT", "DELETE");
+                registry.addMapping("/*").allowedOrigins("https://frontendgnb.web.app").allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
     }
