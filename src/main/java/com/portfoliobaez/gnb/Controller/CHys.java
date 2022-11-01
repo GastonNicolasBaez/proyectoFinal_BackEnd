d592c77e6941cdf9c948120e872552918ac9ca50
GastonNicolasBaez <115502959+GastonNicolasBaez@users.noreply.github.com>
2022-10-31 22:33:30 -0300

    123

--- src/main/java/com/portfoliobaez/gnb/Controller/CHys.java
+++ src/main/java/com/portfoliobaez/gnb/Controller/CHys.java
@@ -24,9 +24,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin (origins = "https://frontendgnb.web.app")
-@RequestMapping("/hys")
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin (origins = "https://frontendgnb.web.app")
+@RequestMapping("/skill")
 public class CHys {
 
     @Autowired
